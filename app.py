1648ea8ea1cec7f9fe1890ecfd69758f7e2236bd
s21840 <80449550+s21840@users.noreply.github.com>
2022-06-27 22:27:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import pandas as pd
 
 filename = "forest_model.sv"
-forest_model = pickle.load(open(filename,'rb'))
+tree_model = pickle.load(open(filename,'rb'))
 base_data = pd.read_csv("card_transactions_400k.csv")
 cols = ["fraud", "distance_from_home", "ratio_to_median_purchase_price", "used_chip", "used_pin_number", "online_order"]
 data = base_data[cols].copy()
@@ -43,8 +43,8 @@ def main():
 		median_purchase_ratio_slider = st.slider("Ratio: ", min_value=int(dataframe["ratio_to_median_purchase_price"].min()), max_value=int(dataframe["ratio_to_median_purchase_price"].max()))
 
 	data = [[used_chip_radio,  used_pin_radio, online_order_radio, distance_home_slider, median_purchase_ratio_slider]]
-	fraudulent = forest_model.predict(data)
-	s_confidence = forest_model.predict_proba(data)
+	fraudulent = tree_model.predict(data)
+	s_confidence = tree_model.predict_proba(data)
 
 	with prediction:
 		st.subheader("Czy podejrzewamy falszywą transakcję?")
